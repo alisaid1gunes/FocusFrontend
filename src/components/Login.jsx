364caa4cd7d75890e619aaea8032b592ce22fa0a
alisaidgunes <alisaidgunes1@gmail.com>
2022-06-08 22:17:04 +0300

    Login Page ui update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -23,23 +23,23 @@ export default function Login() {
     <div className='container-lg bg-black-special flex flex-col w-full min-h-screen overflow-x-hidden justify-center items-center'>
 
       <h1 className='text-white flex justify-center text-5xl mb-20'>Log in.</h1>
-        <div className='w-64 h-10 rounded-lg bg-gradient-to-l from-orange-special to-purple-special text-white border border-1 flex justify-center p-2'>
+        <div className='w-80 h-10 rounded-lg bg-black-special text-white border border-white border-opacity-40  flex justify-center p-2'>
           <svg className="h-6 w-6 text-white" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z"/>
             <path d="M17.788 5.108A9 9 0 1021 12h-8"/>
           </svg>
-          <p className='ml-1'>Continue with Google </p>
+          <p className='ml-2'>Continue with Google </p>
         </div>
-        <h2 className='text-white mb-1 mt-1'>or</h2>
-      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center' >
+        <h2 className='text-white mb-2 mt-2'>or</h2>
+      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center w-full ' >
         <input
           type='text'
           name='username'
           value={email}
           placeholder='Email'
           onChange={(e) => setEmail(e.target.value)}
-          className='w-64 h-10 rounded-lg bg-black-special text-white border border-1 p-2 mb-4'
+          className=' w-80 h-10 rounded-lg bg-black-special text-white border-white border border-opacity-40  p-2 mb-4 w-1/5'
         />
 
         <input
@@ -48,15 +48,15 @@ export default function Login() {
           value={password}
           placeholder='Password'
           onChange={(e) => setPassword(e.target.value)}
-          className='w-64 h-10 rounded-lg bg-black-special text-white border border-1 p-2 mb-6'
+          className='w-80 h-10 rounded-lg bg-black-special text-white border border-white border-opacity-40  p-2 mb-6'
         />
-        <input type='submit' name='submit' className='w-64 h-10 rounded-lg bg-gradient-to-l from-orange-special to-purple-special text-white border border-1 p-2 mb-3' />
+        <input type='submit' name='submit' className='w-80 h-10 rounded-lg bg-gradient-to-l from-orange-special to-purple-special text-white p-2 mb-3' />
       </form>
-      <div className='flex'>
+      <div className='flex mt-2'>
         <p className='text-white text-opacity-60 mr-2'>Don't you have an account?</p>
         <p className='text-white mr-1'>Create Account</p>
       </div>
-      <p className='text-white'>Forgot Password?</p>
+      <p className='text-white mt-2'>Forgot Password?</p>
 
       {error && <p>{message}</p>}
 
